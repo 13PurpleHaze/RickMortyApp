b13c6a012d808d325a491a4ab7dc696ff1571594
13PurpleHaze <kowalskiczeslaw399@gmail.com>
2025-09-15 14:20:10 +0500

    feature/architecture add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:rick_morty_app/app/app.dart';
+import 'package:rick_morty_app/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load(fileName: ".env");
   final preferences = await SharedPreferences.getInstance();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(App(preferences: preferences));
 }
