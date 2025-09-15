8562caacd10a1d967096463680980eb9eaf5c795
13PurpleHaze <kowalskiczeslaw399@gmail.com>
2025-09-15 13:24:49 +0500

    feature/architecture add ap icon, add env package

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:rick_morty_app/app/app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load(fileName: ".env");
   final preferences = await SharedPreferences.getInstance();
+
   runApp(App(preferences: preferences));
 }
