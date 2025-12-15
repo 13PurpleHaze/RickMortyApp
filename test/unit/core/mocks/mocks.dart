fdf8cb93a73a0ea33bc37688289d47147a5961ca
13PurpleHaze <kowalskiczeslaw399@gmail.com>
2025-12-15 13:41:55 +0500

    feature/tests add characters_bloc tests, fix mocks, add character_fixture

--- test/unit/core/mocks/mocks.dart
+++ test/unit/core/mocks/mocks.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:mockito/annotations.dart';
+import 'package:rick_morty_app/features/character/character.dart';
 
-@GenerateMocks([Dio])
+@GenerateMocks([Dio, CharacterRepository])
 void main() {}
