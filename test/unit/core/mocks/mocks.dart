9f66f72c03c05d40c4004e2edd9e0ec51ac24d76
13PurpleHaze <kowalskiczeslaw399@gmail.com>
2025-12-15 14:33:17 +0500

    feature/tests add theme_cubit tests

--- test/unit/core/mocks/mocks.dart
+++ test/unit/core/mocks/mocks.dart
@@ -2,6 +2,12 @@ import 'package:dio/dio.dart';
 import 'package:mockito/annotations.dart';
 import 'package:rick_morty_app/features/character/character.dart';
 import 'package:rick_morty_app/features/episodes/episodes.dart';
+import 'package:rick_morty_app/features/settings/settings.dart';
 
-@GenerateMocks([Dio, CharacterRepository, EpisodeRepository])
+@GenerateMocks([
+  Dio,
+  CharacterRepository,
+  EpisodeRepository,
+  SettingsRepository,
+])
 void main() {}
